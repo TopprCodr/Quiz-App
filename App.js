542b8c4f9f1d61810c877b91db3386c2baeaf842
adityasuman2025 <adityasuman2025@gmail.com>
2020-11-26 19:30:41 +0530

    lottie animation added

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 import Login from "./src/Login";
 import SignUp from "./src/SignUp";
+import Landing from "./src/Landing";
 
 export default function App() {
   return (
-    <SignUp />
+    <Landing />
   );
 }
