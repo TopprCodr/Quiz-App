aa93040053da6230464f09625dab0f7b627bbe84
adityasuman2025 <adityasuman2025@gmail.com>
2020-11-30 14:25:14 +0530

    profile form ui created

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import Login from "./src/Login";
 import SignUp from "./src/SignUp";
 import Landing from "./src/Landing";
 import Media from "./src/Media";
+import Profile from "./src/Profile";
 
 export default function App() {
   return (
-    <Media />
+    <Profile />
   );
 }
