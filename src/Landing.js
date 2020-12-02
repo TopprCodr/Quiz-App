a2be041e9d976fe9339efc66b739c6f2ddbc11c0
adityasuman2025 <adityasuman2025@gmail.com>
2020-12-02 18:40:00 +0530

    lottie removed

--- src/Landing.js
+++ src/Landing.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LottieView from 'lottie-react-native';
 
 import BasicButton from "./BasicButton";
 
-export default function Landing({navigation}) {
+export default function Landing({ navigation }) {
     //function to handle when login btn is clicked on
     function handleLoginBtnClick() {
         console.log("login clicked");
@@ -20,8 +19,6 @@ export default function Landing({navigation}) {
     //component rendering
     return (
         <View style={styles.container}>
-            
-
             <Text style={styles.title}>Quizmania</Text>
             <View style={styles.divider}></View>
             <BasicButton
