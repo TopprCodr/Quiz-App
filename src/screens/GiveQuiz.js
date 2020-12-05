45999de66921587df52f13cf40a9be6b5916188a
adityasuman2025 <adityasuman2025@gmail.com>
2020-12-05 20:06:49 +0530

    done for now

--- src/screens/GiveQuiz.js
+++ src/screens/GiveQuiz.js
@@ -11,7 +11,7 @@ export default function GiveQuiz({ route: {
         quizId,
         quizImgUri,
         quizName,
-        questions,
+        questions = [],
     } = {},
 } = {},
     navigation,
